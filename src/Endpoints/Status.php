ef8608d05374a008e07ca014550efd943950cb29
Alexandre LE CORRE <a.Elreco@gmail.com>
2019-08-23 13:49:11 +0000

    Apply fixes from StyleCI

--- src/Endpoints/Status.php
+++ src/Endpoints/Status.php
@@ -4,76 +4,70 @@ namespace elreco\LaravelFortnite\Endpoints;
 
 class Status
 {
-	private $status = null;
+    private $status = null;
 
-	public function __construct($client)
-	{
-		$this->Client = $client;
-	}
+    public function __construct($client)
+    {
+        $this->Client = $client;
+    }
 
-	/*
-	 * Server status (UP or DOWN)
-	 */
-	public function status()
-	{
-		$this->fetch();
+    /*
+     * Server status (UP or DOWN)
+     */
+    public function status()
+    {
+        $this->fetch();
 
-		return $this->status->status;
-	}
+        return $this->status->status;
+    }
 
-	/*
-	 * Server message (Directly from Epic Games)
-	 */
-	public function message()
-	{
-		$this->fetch();
+    /*
+     * Server message (Directly from Epic Games)
+     */
+    public function message()
+    {
+        $this->fetch();
 
-		return $this->status->message;
-	}
+        return $this->status->message;
+    }
 
-	/*
-	 * Fortnite version (something like v4.0, v4.1 etc.)
-	 */
-	public function version()
-	{
-		$this->fetch();
+    /*
+     * Fortnite version (something like v4.0, v4.1 etc.)
+     */
+    public function version()
+    {
+        $this->fetch();
 
-		return $this->status->version;
-	}
+        return $this->status->version;
+    }
 
-	/*
-	 * Get time.
-	 * type: since or duration
-	 * option: seconds
-	 */
-	public function time($type = 'since', $option = 'seconds')
-	{
-		$this->fetch();
+    /*
+     * Get time.
+     * type: since or duration
+     * option: seconds
+     */
+    public function time($type = 'since', $option = 'seconds')
+    {
+        $this->fetch();
 
-		return $this->status->time->$type->$option;
-	}
+        return $this->status->time->$type->$option;
+    }
 
-	/*
-	 * Fetching the current server status.
-	 */
-	public function fetch()
-	{
-		if($this->status == null)
-		{
-			$return = json_decode($this->Client->httpCall('status/fortnite_server_status', []));
+    /*
+     * Fetching the current server status.
+     */
+    public function fetch()
+    {
+        if ($this->status == null) {
+            $return = json_decode($this->Client->httpCall('status/fortnite_server_status', []));
 
-			if(isset($return->error))
-			{
-				FortniteClient::Exception($return->errorMessage);
-			}
-			else
-			{
-				$this->status = $return;
+            if (isset($return->error)) {
+                FortniteClient::Exception($return->errorMessage);
+            } else {
+                $this->status = $return;
 
-				return $return;
-			}
-		}
-	}
+                return $return;
+            }
+        }
+    }
 }
-
-?>
--- src/Endpoints/Weapons.php
+++ src/Endpoints/Weapons.php
@@ -4,24 +4,19 @@ namespace elreco\LaravelFortnite\Endpoints;
 
 class Weapons
 {
-	public function __construct($client)
-	{
-		$this->Client = $client;
-	}
+    public function __construct($client)
+    {
+        $this->Client = $client;
+    }
 
-	public function get()
-	{
-		$return = json_decode($this->Client->httpCall('weapons/get', []));
+    public function get()
+    {
+        $return = json_decode($this->Client->httpCall('weapons/get', []));
 
-		if(isset($return->error))
-		{
-			return $return->errorMessage;
-		}
-		else
-		{
-			return $return;
-		}
-	}
+        if (isset($return->error)) {
+            return $return->errorMessage;
+        } else {
+            return $return;
+        }
+    }
 }
-
-?>
