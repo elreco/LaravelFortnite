84751d7cd929e0dcf979dba9fbc262eab4008f01
Alexandre LE CORRE <a.Elreco@gmail.com>
2019-08-23 13:50:19 +0000

    Apply fixes from StyleCI

--- src/Endpoints/Challenges.php
+++ src/Endpoints/Challenges.php
@@ -4,31 +4,23 @@ namespace elreco\LaravelFortnite\Endpoints;
 
 class Challenges
 {
-	public function __construct($client)
-	{
-		$this->Client = $client;
-	}
+    public function __construct($client)
+    {
+        $this->Client = $client;
+    }
 
-	public function get($season = 'season4', $language = 'en')
-	{
-		if(empty($season) || empty($language))
-		{
-			return 'Please enter a season and language.';
-		}
-		else
-		{
-			$return = json_decode($this->Client->httpCall('challenges/get', ['season' => $season, 'language' => $language]));
+    public function get($season = 'season4', $language = 'en')
+    {
+        if (empty($season) || empty($language)) {
+            return 'Please enter a season and language.';
+        } else {
+            $return = json_decode($this->Client->httpCall('challenges/get', ['season' => $season, 'language' => $language]));
 
-			if(isset($return->error))
-			{
-				return $return->errorMessage;
-			}
-			else
-			{
-				return $return;
-			}
-		}
-	}
+            if (isset($return->error)) {
+                return $return->errorMessage;
+            } else {
+                return $return;
+            }
+        }
+    }
 }
-
-?>
